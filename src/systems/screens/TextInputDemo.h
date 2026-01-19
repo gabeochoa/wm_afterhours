84a47e88620b05f7326ac671db72c95e62c27f90
Gabe Ochoa <gabeochoa@meta.com>
2026-01-19 01:20:13 -0600

    refactor: update TextInputDemo to use standalone text_input plugin
    - Add explicit include for text_input/text_input.h
    - Update afterhours submodule with text_input plugin extraction

--- src/systems/screens/TextInputDemo.h
+++ src/systems/screens/TextInputDemo.h
@@ -5,6 +5,7 @@
 #include "../../theme_presets.h"
 #include "../ExampleScreenRegistry.h"
 #include <afterhours/ah.h>
+#include <afterhours/src/plugins/text_input/text_input.h>
 
 using namespace afterhours::ui;
 using namespace afterhours::ui::imm;
